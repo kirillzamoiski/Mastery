1a030f9e062f9cdafca65a29ed03cb580733c8c8
kirylzamoiski <kirillzamoiski@gmail.com>
2020-02-12 16:14:47 +0300

    Check Jenkins Work

--- service/src/test/java/com/zamoiski/rest/EmployeeRestControllerTest.java
+++ service/src/test/java/com/zamoiski/rest/EmployeeRestControllerTest.java
@@ -84,6 +84,43 @@ class EmployeeRestControllerTest {
         verify(service, times(1)).findById(1L);
     }
 
+
+//    @Test
+//    void addEmployee() throws Exception {
+//
+//        Department department = new Department(2L,"FRP",LocalDateTime.now());
+//        String employee = new ObjectMapper().writeValueAsString(new Employee(3L,"Alice","Petrova", JobTitle.HR,
+//                "FEMALE", LocalDateTime.now(),department));
+//
+//        service.save(any(Employee.class));
+//
+//        mockMvc.perform(
+//                post("/api/employees")
+//                        .accept(MediaType.APPLICATION_JSON)
+//                        .content(employee)
+//                        .contentType(MediaType.APPLICATION_JSON_UTF8))
+//                .andExpect(status().isOk());
+//
+//        verify(service).save(any(Employee.class));
+//    }
+
+//    @Test
+//    void updateEmployee() throws Exception {
+//        String employee = new ObjectMapper().writeValueAsString(new Employee(1L,"Alice","Petrova", JobTitle.HR,
+//                "FEMALE", LocalDateTime.now(),null));
+//
+//        service.save(any(Employee.class));
+//
+//        mockMvc.perform(
+//                put("/api/employees")
+//                        .accept(MediaType.APPLICATION_JSON)
+//                        .content(employee)
+//                        .contentType(MediaType.APPLICATION_JSON_UTF8))
+//                .andExpect(status().isOk());
+//
+//        verify(service).save(any(Employee.class));
+//    }
+
     @Test
     void deleteEmployee() throws Exception {
         service.deleteById(anyLong());
