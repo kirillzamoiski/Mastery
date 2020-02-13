83930d5b012d4037d024c22e330b9d5a4a7825ab
kirylzamoiski <kirillzamoiski@gmail.com>
2020-02-13 16:01:34 +0300

    test jenkins

--- web-app/src/main/java/com/zamoiski/WebApp.java
+++ web-app/src/main/java/com/zamoiski/WebApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebApp {
-    public static void main(String[] args) {
-        SpringApplication.run(WebApp.class,args);
+    public static void main(String[] args) { SpringApplication.run(WebApp.class,args);
     }
 }
