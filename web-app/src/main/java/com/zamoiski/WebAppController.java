4accb8ee331f12e8e798381e47973eaf2b4e2ffb
kirylzamoiski <kirillzamoiski@gmail.com>
2020-02-13 16:52:31 +0300

    Add Logger

--- web-app/src/main/java/com/zamoiski/WebAppController.java
+++ web-app/src/main/java/com/zamoiski/WebAppController.java
@@ -1,6 +1,8 @@
 package com.zamoiski;
 
 import com.zamoiski.model.Employee;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,8 @@ import javax.validation.Valid;
 @RequestMapping("/webapp")
 public class WebAppController {
 
+    private static final Logger LOGGER = LogManager.getLogger(WebAppController.class);
+
     private EmployeeServiceWeb employeeServiceWeb;
     private DepartmentServiceWeb departmentServiceWeb;
 
@@ -22,11 +26,13 @@ public class WebAppController {
 
     @RequestMapping(value = "/")
     public String getHomePage(){
+        LOGGER.info("View web-app home page");
         return "index";
     }
 
     @GetMapping(value = "/employees")
     public ModelAndView getEmployees() {
+        LOGGER.info("View all employees");
         ModelAndView modelAndView = new ModelAndView("viewEmployees");
         modelAndView.addObject("employees", employeeServiceWeb.findAll());
         return modelAndView;
@@ -34,6 +40,7 @@ public class WebAppController {
 
     @GetMapping(value = "/employees/update/{id}")
     public ModelAndView updateEmployee(@PathVariable("id") Long id,Employee employee) {
+        LOGGER.info("Open form for update employee id: " + id);
         ModelAndView modelAndView = new ModelAndView("addupdateEmployee");
         modelAndView.addObject("employee_id",id);
         modelAndView.addObject("departments",departmentServiceWeb.findAll());
@@ -42,6 +49,7 @@ public class WebAppController {
 
     @GetMapping(value = "/employees/add")
     public ModelAndView addEmployee(Employee employee) {
+        LOGGER.info("Open form for new employee");
         ModelAndView modelAndView = new ModelAndView("addupdateEmployee");
         modelAndView.addObject("departments",departmentServiceWeb.findAll());
         return modelAndView;
@@ -49,11 +57,11 @@ public class WebAppController {
 
     @PostMapping(value = "/employees/put/{id}")
     public ModelAndView updateEmployee(@PathVariable("id") Long id,@RequestParam(value = "departmentID") String departmentID,@Valid Employee employee, BindingResult bindingResult) {
+        LOGGER.info("Send backend service update employee");
         employee.setDepartment(departmentServiceWeb.findById(Long.parseLong(departmentID)));
         if (bindingResult.hasErrors()) {
            return updateEmployee(id,employee);
         }
-
 
         employeeServiceWeb.update(id, employee);
         return getEmployees();
@@ -61,6 +69,7 @@ public class WebAppController {
 
     @PostMapping(value = "/employees/put")
     public ModelAndView addEmployee(@RequestParam(value = "departmentID") String departmentID,@Valid Employee employee, BindingResult bindingResult) {
+        LOGGER.info("Send backend service new employee");
         if (bindingResult.hasErrors()) {
             return new ModelAndView("addupdateEmployee");
         }
@@ -71,6 +80,7 @@ public class WebAppController {
 
     @GetMapping(value = "/employees/delete/{id}")
     public ModelAndView deleteEmployee(@PathVariable("id") Long id) {
+        LOGGER.info("Delete employee id:" + id);
         employeeServiceWeb.deleteById(id);
         return getEmployees();
     }
